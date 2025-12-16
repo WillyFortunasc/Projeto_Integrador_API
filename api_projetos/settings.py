f0acb0b316dab59944e58033a069e44e4f248ba1
Willy Fortuna <willyfortunasc.unb@gmail.com>
2025-12-16 12:56:30 -0300

    atualização

--- api_projetos/settings.py
+++ api_projetos/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = "django-insecure-o+%fzr#y278$wd$to#a_eg2ee!(@#pkx3g^bor1)+cv*3docd^"
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 
